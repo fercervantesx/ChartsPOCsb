62433048e8a747894c9d2f5ea216893469a69c26
Fernando Cervantes <fernando.cervantes@wizeline.com>
2019-08-16 10:55:31 -0500

    First commit

---
+++ src/semi-clouds.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default () => (<svg width="22" height="21" viewBox="0 0 22 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M9.10082 15.471C9.10082 16.0629 8.59739 16.528 7.95666 16.528C7.31593 16.528 6.8125 16.0629 6.8125 15.471C6.8125 14.8791 7.95666 13.5684 7.95666 13.5684C7.95666 13.5684 9.10082 14.8791 9.10082 15.471Z" stroke="#92BCCD" stroke-width="1.3" stroke-miterlimit="10" stroke-linecap="round" />
+  <path d="M16.4231 18.8528C16.4231 19.4448 15.9197 19.9099 15.2789 19.9099C14.6382 19.9099 14.1348 19.4448 14.1348 18.8528C14.1348 18.2609 15.2789 16.9502 15.2789 16.9502C15.2789 16.9502 16.4231 18.2609 16.4231 18.8528Z" stroke="#92BCCD" stroke-width="1.3" stroke-miterlimit="10" stroke-linecap="round" />
+  <path d="M14.136 15.471C14.136 16.0629 13.6325 16.528 12.9918 16.528C12.3511 16.528 11.8477 16.0629 11.8477 15.471C11.8477 14.8791 12.9918 13.5684 12.9918 13.5684C12.9918 13.5684 14.136 14.8791 14.136 15.471Z" stroke="#92BCCD" stroke-width="1.3" stroke-miterlimit="10" stroke-linecap="round" />
+  <path d="M11.8469 18.8528C11.8469 19.4448 11.3435 19.9099 10.7028 19.9099C10.062 19.9099 9.55859 19.4448 9.55859 18.8528C9.55859 18.2609 10.7028 16.9502 10.7028 16.9502C10.7028 16.9502 11.8469 18.2609 11.8469 18.8528Z" stroke="#92BCCD" stroke-width="1.3" stroke-miterlimit="10" stroke-linecap="round" />
+  <path d="M2.73902 4.47876L1.64062 4.01367" stroke="#FFE600" stroke-width="1.3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+  <path d="M8.32324 2.53232L8.68937 1.51758" stroke="#FFE600" stroke-width="1.3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+  <path d="M2.14416 7.65039L1 7.77723" stroke="#FFE600" stroke-width="1.3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+  <path d="M5.0729 2.53232L4.61523 1.51758" stroke="#FFE600" stroke-width="1.3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+  <path d="M16.6524 16.1032C19.0323 16.1032 21.0002 14.2851 21.0002 12.0865C21.0002 10.7335 20.268 9.54968 19.1696 8.78862C19.1696 8.70406 19.1696 8.57721 19.1696 8.49265C19.1696 5.70211 16.6982 3.41895 13.6776 3.41895C10.657 3.41895 8.18563 5.70211 8.18563 8.49265C8.18563 9.00002 8.27716 9.50739 8.41446 9.97248C7.72796 9.33827 6.76687 8.91546 5.66847 8.91546C3.51745 8.91546 1.77832 10.5221 1.77832 12.5093C1.77832 14.4965 3.51745 16.1032 5.66847 16.1032" stroke="#92BCCD" stroke-width="1.3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+</svg>
+)
